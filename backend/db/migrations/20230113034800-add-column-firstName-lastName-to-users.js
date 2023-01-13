9cf627977c8d34e507e1279fe8a1b782a8f4d072
Dean <dcth628@gmail.com>
2023-01-12 21:54:41 -0800

    added options

--- backend/db/migrations/20230113034800-add-column-firstName-lastName-to-users.js
+++ backend/db/migrations/20230113034800-add-column-firstName-lastName-to-users.js
@@ -1,7 +1,7 @@
 'use strict';
-let options = {}
+let options = {};
 if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+  options.schema = process.env.SCHEMA;
 }
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
