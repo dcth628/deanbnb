9669f5725fbc3a8205e093793687b304dd406d33
Dean <dcth628@gmail.com>
2023-01-12 20:40:07 -0800

    added columns firstName lastName

---
+++ backend/db/migrations/20230113034800-add-column-firstName-lastName-to-users.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn(
+      'Users',
+      'firstName',
+      {
+        type: Sequelize.STRING(30),
+        allowNull: false
+      })
+
+      await queryInterface.addColumn(
+        'Users',
+        'lastName',
+        {
+          type: Sequelize.STRING(30),
+          allowNull: false
+        }
+      )
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Users', 'firstName');
+    await queryInterface.removeColumn('Users', 'lastName');
+  }
+};
--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,9 +2,23 @@ const express = require('express');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
 
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
         const { credential, password } = req.body;
 
@@ -23,6 +37,28 @@ router.post(
         return res.json({
             user
         });
+    }
+);
+
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+    }
+);
+
+// Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({});
     }
 );
 
