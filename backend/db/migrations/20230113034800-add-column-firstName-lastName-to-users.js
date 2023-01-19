604858b85dc15af3628775e1e8353fcedd32d0ae
Dean <dcth628@gmail.com>
2023-01-18 19:56:00 -0800

    added columns to users

--- backend/db/migrations/20230113034800-add-column-firstName-lastName-to-users.js
+++ backend/db/migrations/20230113034800-add-column-firstName-lastName-to-users.js
@@ -1,26 +1,31 @@
 'use strict';
-
+let options = {};
+options.tableName= "Users"
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.addColumn(
-      'Users',
+      options,
       'firstName',
       {
         type: Sequelize.STRING(30),
+        allowNull: false
       })
 
       await queryInterface.addColumn(
-        'Users',
+        options,
         'lastName',
         {
           type: Sequelize.STRING(30),
-        }
-      )
+          allowNull: false
+        })
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Users', 'firstName');
-    await queryInterface.removeColumn('Users', 'lastName');
+    await queryInterface.removeColumn(options, 'firstName');
+    await queryInterface.removeColumn(options, 'lastName');
   }
 };
