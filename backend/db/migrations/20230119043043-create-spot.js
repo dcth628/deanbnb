45de7a4a072fe100af456d9d95ff8250b51816e7
Dean <dcth628@gmail.com>
2023-01-27 16:55:59 -0800

    replaced table names to options

--- backend/db/migrations/20230119043043-create-spot.js
+++ backend/db/migrations/20230119043043-create-spot.js
@@ -1,8 +1,14 @@
 'use strict';
+let options = {};
+options.tableName= "Spots"
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Spots', {
+    await queryInterface.createTable(options, {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -69,6 +75,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Spots');
+    await queryInterface.dropTable(options);
   }
 };
--- backend/db/migrations/20230119043202-create-image.js
+++ backend/db/migrations/20230119043202-create-image.js
@@ -1,8 +1,13 @@
 'use strict';
+let options = {};
+options.tableName= "Images"
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Images', {
+    await queryInterface.createTable(options, {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -44,6 +49,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Images');
+    await queryInterface.dropTable(options);
   }
 };
