1b3fe97aae4d606cc2caa843ecd76e07cf848fda
Dean <dcth628@gmail.com>
2023-02-23 20:39:34 -0800

    finished authenticate me mimus bonus

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
 router.get('/hello/world', function (req, res) {
     res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -46,7 +47,7 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV !== 'production') {
     router.get('/api/csrf/restore', (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.json({});
+        return res.status(201).json({});
     });
 };
 
