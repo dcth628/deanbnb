03789b07b1b1a3c7b54b65b6a1ee56559c9aa331
Dean <dcth628@gmail.com>
2023-02-24 13:04:40 -0800

    login css

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -7,9 +7,9 @@ import './LoginForm.css'
 const LoginFormPage = () => {
     const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
-    const [ credential, setCredential ] = useState('');
-    const [ password, setPasswrod ] = useState('');
-    const [ errors, setErrors ] = useState([]);
+    const [credential, setCredential] = useState('');
+    const [password, setPasswrod] = useState('');
+    const [errors, setErrors] = useState([]);
 
     if (sessionUser) return (
         <Redirect to="/" />
@@ -18,7 +18,7 @@ const LoginFormPage = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         setErrors([]);
-        return dispatch(sessionActions.login({credential, password}))
+        return dispatch(sessionActions.login({ credential, password }))
             .catch(async (res) => {
                 const data = await res.json();
                 if (data && data.errors) setErrors(data.errors);
@@ -27,33 +27,38 @@ const LoginFormPage = () => {
 
     return (
         <form
-        className="singin-form"
-        onSubmit={handleSubmit}>
+            className="signin-form"
+            onSubmit={handleSubmit}>
             <ul>
                 {errors.map((error, idx) =>
-                <li key={idx}>{error}</li>
+                    <li key={idx}>{error}</li>
                 )}
             </ul>
-            <label className="username">
-                Username or Email
-                <input
-                    className='input'
-                    type="text"
-                    value={credential}
-                    onChange={(e) => setCredential(e.target.value)}
-                    required
+            <div className="username-input">
+                <label className="username">
+                    Username or Email
+                    <input
+                        className='input'
+                        type="text"
+                        value={credential}
+                        onChange={(e) => setCredential(e.target.value)}
+                        required
                     />
-            </label>
-            <label className="password">
-                Passwrod
-                <input
-                className='input'
-                type="password"
-                value={password}
-                onChange={(e) => setPasswrod(e.target.value)}
-                required
-                />
-            </label>
+                </label>
+            </div>
+            <div className="password-input">
+                <label className="password">
+
+                    Passwrod
+                    <input
+                        className='input'
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPasswrod(e.target.value)}
+                        required
+                    />
+                </label>
+            </div>
             <button className="login-button" type="submit">Log In</button>
         </form>
 
