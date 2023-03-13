cf3be9e7c6e126fc26fbb735e89d7b1c3c3d908f
Dean <dcth628@gmail.com>
2023-03-12 18:12:11 -0700

    delete console log

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -17,7 +17,6 @@ const SpotDetails = () => {
   const reviews = useSelector(state => state.review);
   let sessionUserReview;
   if (sessionUser) sessionUserReview = Object.values(reviews).filter(review => review.userId === sessionUser.id)
-  console.log(sessionUserReview , 'this is session user review')
 
 
   useEffect(() => {
